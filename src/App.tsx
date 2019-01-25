e3335500c52a3e99e7567a5e80f8e1345e24a79c
Bryan Wolfe <bryan.d.wolfe1@gmail.com>
2019-01-25 15:06:52 -0800

    A fun little website to brush up on react with typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Main />
       </div>
     );
   }
+}
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>{document.title}</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main className="App-main">
+      <img src={logo} className="App-logo" alt="logo" />
+    </main>
+  );
 }
 
 export default App;
