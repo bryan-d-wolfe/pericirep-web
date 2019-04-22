4b7bc88fac91c61d01cc4c8178bc3088f80e62ce
Bryan Wolfe <bryan.d.wolfe1@gmail.com>
2019-04-21 21:44:16 -0700

    Added type for onChange event.

--- src/Elements/Form.tsx
+++ src/Elements/Form.tsx
@@ -1,14 +1,21 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent } from 'react';
 
 function Form () {
-  const [guess, setGuess] = useState('');
+  const [text, setText] = useState('');
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
-    setGuess('Wrong!');
+    setText('Wrong Word!');
   } 
+  function onChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    e.preventDefault();
+    setText(e.currentTarget.value);
+  }
   return (
     <form>
-      <textarea onChange={(e) => setGuess(e.target.value)} value={guess} />
+      <textarea 
+        onChange={(e: ChangeEvent<HTMLTextAreaElement>) => onChange(e)}
+        value={text}
+      />
       <button onClick={(e: FormEvent) => handleSubmit(e)}>Submit!</button>
     </form>
   );
